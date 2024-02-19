99ce78331bc8f3ae2b28d9e5abd3dcbca10a7602
Alex C-G <alexcg@outlook.com>
2024-02-19 16:22:43 +0100

    feat: specify shoptimizer backend url
    Signed-off-by: Alex C-G <alexcg@outlook.com>

--- frontend/helper.py
+++ frontend/helper.py
@@ -3,12 +3,13 @@ import os
 
 def get_settings():
     settings = {
-        "scenex_api_key": os.environ.get("scenex_api_key", ""),
-        "woocommerce_url": os.environ.get("woocommerce_url", ""),
-        "woocommerce_key": os.environ.get("woocommerce_key", ""),
-        "woocommerce_secret": os.environ.get("woocommerce_secret", ""),
-        "shopify_shop_name": os.environ.get("shopify_shop_name", ""),
-        "shopify_access_token": os.environ.get("shopify_access_token", ""),
+        "shoptimizer_backend_url": os.environ.get("SHOPTIMIZER_BACKEND_URL", ""),
+        "scenex_api_key": os.environ.get("SCENEX_API_KEY", ""),
+        "woocommerce_url": os.environ.get("WOOCOMMERCE_URL", ""),
+        "woocommerce_key": os.environ.get("WOOCOMMERCE_KEY", ""),
+        "woocommerce_secret": os.environ.get("WOOCOMMERCE_SECRET", ""),
+        "shopify_shop_name": os.environ.get("SHOPIFY_SHOP_NAME", ""),
+        "shopify_access_token": os.environ.get("SHOPIFY_ACCESS_TOKEN", ""),
     }
 
     return settings
