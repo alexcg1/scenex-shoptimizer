96cd7ded0932f05322cb4219a0e61eec84e5f0da
Alex C-G <alexcg@outlook.com>
2024-02-20 14:43:03 +0100

    fix: remove invalid creds import

---
+++ backend/creds.py
@@ -0,0 +1,27 @@
+# scenex
+SCENEX_API_KEY = "fuHUDVCTWJAAGQwMHWSV:9b2d61d69d31ee35d51e56a39cddc28e7622dbd8652512a781d95b2a9783683f"
+SCENEX_URL = "https://api.scenex.jina.ai/v1/describe"
+SCENEX_API_KEY = "2XeItpyQoGq7YoPuUtLJ:2e85033c59845441d6eb21bc5de98117460de9b8d9f3befc99221cafd64eeaaa"
+
+# wordpress general
+# need to use app password, not user password here
+WORDPRESS_USER = "admin"
+
+# localhost
+# WORDPRESS_URL = "http://localhost:80"  # local
+# WORDPRESS_PASSWORD = "bTrX QOCS Uybb 0cWF Rklh vIeV"  # local
+# WOOCOMMERCE_KEY = "ck_09a9b119bb4b72e7e3fe6c5d947cf84eff4251c2"
+# WOOCOMMERCE_SECRET = "cs_0403b0985e05697aca30f2e43571ebc3e2f671ea"
+
+
+# aws
+class WooCommerceCreds:
+    WORDPRESS_URL = "http://ec2-16-171-139-144.eu-north-1.compute.amazonaws.com"  # aws
+    WORDPRESS_PASSWORD = "Z7oR 2ieG v0Jy H9dw q7aV RfCt"  # aws
+    WOOCOMMERCE_KEY = "ck_2537510302eb2872d85ddd130f45bbc125e47c06"
+    WOOCOMMERCE_SECRET = "cs_4147d7791c79f3c2d7f9bd2685ad2cb85875e701"
+
+
+# shopify
+SHOPIFY_SHOP_NAME = "84a616-3"
+SHOPIFY_ACCESS_TOKEN = "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"
