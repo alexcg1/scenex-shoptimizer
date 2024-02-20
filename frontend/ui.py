0fe3c2f9a7110aea2e47245708f59488279eb054
Alex C-G <alexcg@outlook.com>
2024-02-20 15:23:41 +0100

    chore: rm junk

--- frontend/ui.py
+++ frontend/ui.py
@@ -3,22 +3,6 @@ from rich.console import Console
 
 console = Console(tab_size=2)
 
-help_markdown = """
-Alt Texter automatically adds alt tags to all the images (including featured images) on your blog using [SceneXplain](https://scenex.jina.ai).
-
-### How does it work?
-
-Alt Texter will:
-
-- Download each content item from whatever platform you're using
-- Check each image to see if it has alt text:
-    - If yes, skip that image
-    - If no, create an alt text using SceneXplain and write that to the content data
-- Check if the content has new alt texts:
-    - If yes, update the content
-    - If no, skip update
-"""
-
 st.set_page_config(page_title="Shoptimizer", layout="wide")
 st.warning(
     "Shoptimizer is still under heavy development. Back up your data before using it!"
